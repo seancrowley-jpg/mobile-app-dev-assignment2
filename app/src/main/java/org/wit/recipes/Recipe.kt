f196b1ba54e175f25f8c6d38208c3c2e3044b4b2
Sean-Crowley <seancrowley033@gmail.com>
2021-10-15 13:11:36 +0100

    First commit - Created new project with Empty Activity template Renamed default activity to Recipe

---
+++ app/src/main/java/org/wit/recipes/Recipe.kt
@@ -0,0 +1,11 @@
+package org.wit.recipes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Recipe : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recipe)
+    }
+}
