a5876c5f2837a49fed9265799b905009f350f208
Sean-Crowley <seancrowley033@gmail.com>
2021-10-15 14:37:59 +0100

    Added logging with Timber

--- app/src/main/java/org/wit/recipes/Recipe.kt
+++ app/src/main/java/org/wit/recipes/Recipe.kt
@@ -2,10 +2,16 @@ package org.wit.recipes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import timber.log.Timber
+import timber.log.Timber.i
 
 class Recipe : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recipe)
+
+        Timber.plant(Timber.DebugTree())
+
+        i("Recipe Activity started..")
     }
 }
