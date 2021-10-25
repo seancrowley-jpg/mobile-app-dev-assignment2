ab7ad07c29c3b6aec67e9fca1ee060cb0f5eca29
Sean-Crowley <seancrowley033@gmail.com>
2021-10-25 16:42:59 +0100

    Added a SplashScreen activity with layout file Displayed before login actitvity

---
+++ app/src/main/java/org/wit/recipes/activities/SplashScreenActivity.kt
@@ -0,0 +1,27 @@
+package org.wit.recipes.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import org.wit.recipes.R
+
+@Suppress("DEPRECATION")
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        Handler().postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
