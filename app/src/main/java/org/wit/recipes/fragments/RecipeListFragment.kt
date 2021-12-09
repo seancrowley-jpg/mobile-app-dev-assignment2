150d4cac45c196fe38cd96ac84c127d80b616bd6
Sean-Crowley <seancrowley033@gmail.com>
2021-12-09 12:55:03 +0000

    Added menu to Recipe List fragment Add search bar and delete all function to menu

--- app/src/main/java/org/wit/recipes/fragments/RecipeListFragment.kt
+++ app/src/main/java/org/wit/recipes/fragments/RecipeListFragment.kt
@@ -1,11 +1,14 @@
 package org.wit.recipes.fragments
 
 import android.os.Bundle
+import android.view.*
+import android.view.inputmethod.EditorInfo
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import androidx.recyclerview.widget.LinearLayoutManager
+import org.wit.recipes.R
 import org.wit.recipes.adapters.RecipeAdapter
 import org.wit.recipes.adapters.RecipeListener
 import org.wit.recipes.databinding.FragmentRecipeListBinding
@@ -16,10 +19,11 @@ class RecipeListFragment : Fragment(), RecipeListener {
     lateinit var app: MainApp
     private var _fragBinding: FragmentRecipeListBinding? = null
     private val fragBinding get() = _fragBinding!!
-    //private lateinit var adapter: RecipeAdapter
+    private lateinit var adapter: RecipeAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         app = activity?.application as MainApp
+        setHasOptionsMenu(true)
         super.onCreate(savedInstanceState)
     }
 
@@ -31,10 +35,40 @@ class RecipeListFragment : Fragment(), RecipeListener {
         val root = fragBinding.root
         activity?.title = "Welcome ${app.currentUser?.name}"
 
-        fragBinding.recyclerView.setLayoutManager(LinearLayoutManager(activity))
-        fragBinding.recyclerView.adapter = RecipeAdapter(app.recipes.findAll(),this)
+        fragBinding.recyclerView.layoutManager = LinearLayoutManager(activity)
+        adapter = RecipeAdapter(app.recipes.findAll(),this)
+        fragBinding.recyclerView.adapter = adapter
 
         return root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_main, menu)
+        val searchItem = menu.findItem(R.id.item_search)
+        val searchView = searchItem?.actionView as SearchView
+        searchView.imeOptions = EditorInfo.IME_ACTION_DONE
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                adapter.filter.filter(p0)
+                //if(p0.isNullOrBlank()) loadRecipes()
+                return true
+            }
+
+        })
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.item_delete_all){
+            app.recipes.deleteAll()
+            fragBinding.recyclerView.adapter?.notifyDataSetChanged()
+        }
+        return NavigationUI.onNavDestinationSelected(item,
+            requireView().findNavController()) || super.onOptionsItemSelected(item)
     }
 
     companion object {
@@ -55,7 +89,8 @@ class RecipeListFragment : Fragment(), RecipeListener {
     }
 
     override fun onDeleteClick(recipe: RecipeModel) {
-        TODO("Not yet implemented")
+        app.recipes.delete(recipe)
+        fragBinding.recyclerView.adapter?.notifyDataSetChanged()
     }
 
     override fun onEditClick(recipe: RecipeModel) {
